a371fdc89fc58017c79249f407c2b7cfb305db5f
sayed mosad <sayedmosad0022@gmail.com>
2026-01-07 17:39:11 +0200

    Add primary button and outlined button widgets for enhanced UI interaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:finance_ui/Core/Style/theme.dart';
+import 'package:finance_ui/Core/widgets/primary_button_widget.dart';
+import 'package:finance_ui/Core/widgets/primary_outlined_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,8 +16,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
-
-      child: MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold()),
+      builder: (context, child) => MaterialApp(
+        theme: AppTheme.lightTheme,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: PrimaryOutlinedButtonWidget(
+              onPressed: () {},
+              buttonText: 'login',
+              fontSize: 20.sp,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
